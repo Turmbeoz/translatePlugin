5e31a02586800c99abceda139748a180106bbd8d
Tom <tomophilia@mac.com>
2021-12-01 20:53:45 -0500

    first commit

---
+++ main.js
@@ -0,0 +1,23 @@
+var unirest = require("unirest");
+var req = unirest("POST", "https://google-translate1.p.rapidapi.com/language/translate/v2");
+
+req.headers({
+	"content-type": "application/x-www-form-urlencoded",
+	"accept-encoding": "application/gzip",
+	"x-rapidapi-key": "4b695a839emsh2bc9a220eb8bb7bp145d02jsnf1d5f4c36f65",
+	"x-rapidapi-host": "google-translate1.p.rapidapi.com",
+	"useQueryString": true
+});
+
+req.form({
+	"q": "Hello, everyone! Today we are going to ",
+	"target": "de",
+	"source": "en"
+});
+
+
+req.end(function (res) {
+	if (res.error) throw new Error(res.error);
+
+	console.log(res.body.data.translations);
+});
